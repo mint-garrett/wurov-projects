29025a98282ecb22410aa44e622694f4e59ae995
Garrett <stmichaelboy@icloud.com>
2026-01-27 20:26:06 -0500

    Add files via upload

---
+++ model_script_copy.py
@@ -0,0 +1,25 @@
+from picamzero import Camera
+import os
+import time
+
+#stores photo folder directory as a string 
+home_dir = os.path.expanduser("~/Documents/test_pics") 
+
+#creates dir if it doesn't exist
+os.makedirs(home_dir, exist_ok=True)
+
+#names camera function
+cam = Camera()
+
+#set cam resolution in px
+cam.resolution = (1280,720)
+
+#saves photo as a jpg with the timestamp as part of the name in the home_dir as a variable
+photo_location = f"{home_dir}/photo_{int(time.time())}.jpg"
+
+cam.start_preview()
+time.sleep(5)
+cam.take_photo(photo_location)
+cam.stop_preview()
+
+print(f"The photo is stored at {photo_location}")
